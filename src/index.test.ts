43890b734cdd71db77a156e94b541eca4b3d57e7
Emilien Domenge <emilien.domenge-heritier@abtasty.com>
2020-05-06 15:21:45 +0200

    add unit tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -5,23 +5,106 @@ describe('FlagshipLogger', () => {
     enableConsoleLogs: true,
   };
   let config: LogConfig;
-  // let spyWarnLogs;
-  // let spyErrorLogs;
-  // let spyFatalLogs;
-  let spyInfoLogs;
-  // let spyDebugLogs;
+  let spyWarnLogs: any;
+  let spyErrorLogs: any;
+  let spyInfoLogs: any;
+
   beforeEach(() => {
     config = defaultConfig as LogConfig;
+    spyWarnLogs = jest.spyOn(console, 'warn').mockImplementation();
+    spyErrorLogs = jest.spyOn(console, 'error').mockImplementation();
+    spyInfoLogs = jest.spyOn(console, 'log').mockImplementation();
+  });
+
+  afterEach(() => {
+    spyWarnLogs.mockRestore();
+    spyErrorLogs.mockRestore();
+    spyInfoLogs.mockRestore();
   });
 
   it('should info log', () => {
     const fsLogger = FlagshipLogger.getLogger(config);
-    spyInfoLogs = jest.spyOn(fsLogger, 'info');
-    // spyWarnLogs = jest.spyOn(fsLogger, 'warn');
-    // spyErrorLogs = jest.spyOn(fsLogger, 'error');
-    // spyFatalLogs = jest.spyOn(fsLogger, 'fatal');
-    // spyDebugLogs = jest.spyOn(fsLogger, 'debug');
+    const spy = jest.spyOn(fsLogger, 'info');
     fsLogger.info('This is a info log');
-    expect(spyInfoLogs).toHaveBeenNthCalledWith(1, 'This is a info log');
+    expect(spy).toHaveBeenNthCalledWith(1, 'This is a info log');
+    const splitElement = spyInfoLogs.mock.calls[0][0].split(' - ');
+    const time = splitElement[0].replace(new RegExp('[0-9]', 'g'), 'x');
+    const name = splitElement[1];
+    const value = splitElement[2];
+    expect(time).toEqual('[xx:xx:xx]');
+    expect(name).toEqual('Flagship SDK');
+    expect(value).toEqual('This is a info log');
+    expect(spyErrorLogs).toHaveBeenCalledTimes(0);
+    expect(spyWarnLogs).toHaveBeenCalledTimes(0);
+  });
+
+  it('should warn log', () => {
+    const fsLogger = FlagshipLogger.getLogger(config);
+    const spy = jest.spyOn(fsLogger, 'warn');
+    const param = 'This is a warn log';
+    fsLogger.warn(param);
+    expect(spy).toHaveBeenNthCalledWith(1, param);
+    const splitElement = spyWarnLogs.mock.calls[0][0].split(' - ');
+    const time = splitElement[0].replace(new RegExp('[0-9]', 'g'), 'x');
+    const name = splitElement[1];
+    const value = splitElement[2];
+    expect(time).toEqual('[xx:xx:xx]');
+    expect(name).toEqual('Flagship SDK');
+    expect(value).toEqual(param);
+    expect(spyErrorLogs).toHaveBeenCalledTimes(0);
+    expect(spyInfoLogs).toHaveBeenCalledTimes(0);
+  });
+
+  it('should error log', () => {
+    const fsLogger = FlagshipLogger.getLogger(config);
+    const spy = jest.spyOn(fsLogger, 'error');
+    const param = 'This is a error log';
+    fsLogger.error(param);
+    expect(spy).toHaveBeenNthCalledWith(1, param);
+    const splitElement = spyErrorLogs.mock.calls[0][0].split(' - ');
+    const time = splitElement[0].replace(new RegExp('[0-9]', 'g'), 'x');
+    const name = splitElement[1];
+    const value = splitElement[2];
+    expect(time).toEqual('[xx:xx:xx]');
+    expect(name).toEqual('Flagship SDK');
+    expect(value).toEqual(param);
+    expect(spyWarnLogs).toHaveBeenCalledTimes(0);
+    expect(spyInfoLogs).toHaveBeenCalledTimes(0);
+  });
+  it('should fatal log', () => {
+    const fsLogger = FlagshipLogger.getLogger(config);
+    const spy = jest.spyOn(fsLogger, 'fatal');
+    const param = 'This is a fatal log';
+    fsLogger.fatal(param);
+    expect(spy).toHaveBeenNthCalledWith(1, param);
+    const splitElement = spyErrorLogs.mock.calls[0][0].split(' - ');
+    const time = splitElement[0].replace(new RegExp('[0-9]', 'g'), 'x');
+    const name = splitElement[1];
+    const type = splitElement[2].split(': ')[0];
+    const value = splitElement[2].split(': ')[1];
+    expect(time).toEqual('[xx:xx:xx]');
+    expect(name).toEqual('Flagship SDK');
+    expect(type).toEqual('Fatal');
+    expect(value).toEqual(param);
+    expect(spyWarnLogs).toHaveBeenCalledTimes(0);
+    expect(spyInfoLogs).toHaveBeenCalledTimes(0);
+  });
+  it('should debug log', () => {
+    const fsLogger = FlagshipLogger.getLogger(config);
+    const spy = jest.spyOn(fsLogger, 'debug');
+    const param = 'This is a debug log';
+    fsLogger.debug(param);
+    expect(spy).toHaveBeenNthCalledWith(1, param);
+    const splitElement = spyInfoLogs.mock.calls[0][0].split(' - ');
+    const time = splitElement[0].replace(new RegExp('[0-9]', 'g'), 'x');
+    const name = splitElement[1];
+    const type = splitElement[2].split(': ')[0];
+    const value = splitElement[2].split(': ')[1];
+    expect(time).toEqual('[xx:xx:xx]');
+    expect(name).toEqual('Flagship SDK');
+    expect(type).toEqual('Debug');
+    expect(value).toEqual(param);
+    expect(spyWarnLogs).toHaveBeenCalledTimes(0);
+    expect(spyErrorLogs).toHaveBeenCalledTimes(0);
   });
 });
